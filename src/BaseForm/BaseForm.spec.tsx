766d2f67dee410ed3a8b67584eb4bb2ec632d519
Ian Schwartz <ischwartz@cargurus.com>
2023-07-31 15:17:29 -0400

    tested

--- src/BaseForm/BaseForm.spec.tsx
+++ src/BaseForm/BaseForm.spec.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event/dist';
-import BaseForm, { buttonText } from './BaseForm';
+import { BaseForm } from './BaseForm';
 
 describe('BaseForm Tests', () => {
     it('should render a BaseForm', () => {
-        const onClick = jest.fn();
-        render(<BaseForm onClick={onClick} />);
+        const onSubmit = jest.fn();
+        render(
+            <BaseForm
+                onSubmit={onSubmit}
+                button={<button type="submit">submit</button>}
+                title="cool"
+            >
+                <input name="foo" />
+                <input name="cool" value="yes" type="checkbox" />
+            </BaseForm>
+        );
 
-        const btn = screen.getByRole('button', { name: buttonText });
-        expect(btn).toBeInTheDocument();
+        const btn = screen.getByRole('button', {
+            name: 'submit',
+        }) as HTMLButtonElement;
+        expect(btn.disabled).toBeTruthy();
 
+        userEvent.type(screen.getByRole('textbox'), 'bar');
+        userEvent.click(screen.getByRole('checkbox'));
+
+        expect(btn.disabled).toBeFalsy();
         userEvent.click(btn);
-        expect(onClick).toHaveBeenCalled();
+
+        expect(onSubmit).toHaveBeenCalledWith({
+            foo: 'bar',
+            cool: 'yes',
+        });
     });
 });
