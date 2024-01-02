3b9a240c11ca8397539a22c3dd9595cf4c06ee8c
Ian Schwartz <ischwartz@cargurus.com>
2024-01-02 09:42:31 -0500

    removed file rename from build script

---
+++ cjs/useIntersectionObserver/index.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useIntersectionObserver = void 0;
+const react_1 = require("react");
+/**
+ * Warps useEffect for functions that are meant to be called when a component is scrolled into view. Does it by
+ * using the IntersectionObserver API to watch a div element for when a certain percentage of it gets into view and
+ * then calls the callback function passed as a parameter to run.
+ * @param callback A callback function to run when the component is in view
+ * @param options A variable that contains the properties: root, rootMargin, and threshold.
+ *      @property root <Element|null>: The element that is used as the viewport for checking visibility of the target.
+ *      @property rootMargin <undefined|string>: Margin around the root.
+ *      @property threshold <number|number[]>: Either a single number or an array of numbers which indicate at what
+ *      percentage of the target's visibility the observer's callback should be executed.
+ */
+const useIntersectionObserver = (callback, options) => {
+    const containerRef = (0, react_1.useRef)(null);
+    (0, react_1.useEffect)(() => {
+        if ('IntersectionObserver' in window) {
+            const observer = new IntersectionObserver(callback, options);
+            const container = containerRef.current;
+            if (container) {
+                observer.observe(container);
+            }
+            return () => {
+                if (container)
+                    observer.unobserve(container);
+            };
+        }
+        else {
+            return () => null;
+        }
+    }, [containerRef, options, callback]);
+    return { containerRef };
+};
+exports.useIntersectionObserver = useIntersectionObserver;
